1eb2c7752b697d0f7d2139f0839e1709f8899b69
Diksha-Rathod <drathod@systango.com>
2022-11-01 19:33:17 +0530

    hello

--- second.js
+++ second.js
@@ -2,6 +2,8 @@ var http=require("http");
 http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/html'});
     res.write("welcome in it park");
-    res.write("hello");
+    res.write(" hello");
+    res.write("  in");
+    res.write(" indore");
 
 }).listen(8080);
