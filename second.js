3955db517a71c0d018791947a70539f69de9ef29
Diksha-Rathod <drathod@systango.com>
2022-11-01 18:56:20 +0530

    second change

--- second.js
+++ second.js
@@ -1,5 +1,5 @@
 var http=require("http");
 http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/html'});
-    res.end("welcome in systango");
+    res.end("welcome in it park");
 }).listen(8080);
